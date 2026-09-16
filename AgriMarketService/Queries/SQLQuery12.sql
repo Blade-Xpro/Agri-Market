fb379a03e4dc9e3c1ea0429667eb2ba1adc9e6ea
reeWoah <tshehlarethakgetse@gmail.com>
2026-09-16 09:12:59 +0200

    Adda discount table

---
+++ AgriMarketService/Queries/SQLQuery12.sql
@@ -0,0 +1,11 @@
+﻿-- Stores discount codes with fixed monetary amounts deducted at checkout.
+CREATE TABLE [dbo].[Discounts]
+(
+    [DiscountId] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [DiscountCode] VARCHAR(50) NOT NULL UNIQUE,
+    [DiscountAmount] DECIMAL(10,2) NOT NULL,
+
+    CHECK (LEN(LTRIM(RTRIM([DiscountCode]))) > 0),
+    CHECK ([DiscountAmount] >= 0)
+);
+
